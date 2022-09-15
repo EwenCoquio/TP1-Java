ce4b3c98e292bfccbbb8442134a60317c022e190
Moi <ewencoquio2389@gmail.com>
2022-09-15 13:12:40 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@
 public class Main {
     public static void main(String[] argv) {
         System.out.println("Hello world!");
-        PileEntTab test = new PileEntTab(100);
-        for (int i = 0; i < argv.length; i++) {
+
+        //PileEntTab test = new PileEntTab(100);
+        /*for (int i = 0; i < argv.length; i++) {
             if (argv[i].equals("e")) {
                 i++;
                 test.empile(Integer.parseInt(argv[i]));
@@ -16,6 +17,18 @@ public class Main {
             }
             else
                 System.out.println("commande incorrecte " + argv[i]);
-        }
+        }*/
+       // MaillonFrele test2 = new MaillonFrele(3);
+        PileEntMaillon test = new PileEntMaillon(2);
+        System.out.println(test.sommet());
+        test.empile(3);
+        System.out.println(test.sommet());
+        test.empile(8);
+        System.out.println(test.sommet());
+        test.depile();
+        test.depile();
+        test.empile(6);
+        test.dump();
+
     }
 }
