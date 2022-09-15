a30fe0db11381faef5bb21ef917053a3f46987e1
Moi <ewencoquio2389@gmail.com>
2022-09-15 09:21:32 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] argv) {
         System.out.println("Hello world!");
+        PileEntTab test = new PileEntTab(100);
+        for (int i = 0; i < argv.length; i++) {
+            if (argv[i].equals("e")) {
+                i++;
+                test.empile(Integer.parseInt(argv[i]));
+            }
+            else if (argv[i].equals("d")) {
+                test.depile();
+            }
+            else if (argv[i].equals("dp")) {
+                test.dump();
+            }
+            else
+                System.out.println("commande incorrecte " + argv[i]);
+        }
     }
 }
