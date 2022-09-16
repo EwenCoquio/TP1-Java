68bfccb6c24175e94ee3dcebb474a3d451c6d873
Ewen Coquio <92462179+EwenCoquio@users.noreply.github.com>
2022-09-16 10:03:13 +0200

    Create PileEnt.java

---
+++ PileEnt.java
@@ -0,0 +1,18 @@
+public interface PileEnt {
+
+    public void empile(int elmt);
+
+    public void depile();
+
+    public void dump();
+
+    public int sommet();
+
+    public boolean estVide();
+
+
+
+
+
+
+}
